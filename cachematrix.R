f077460e3f24e7ca294d23cd9a67cb54557e50ef
tszczurowski <tszczurowski@gmail.com>
2014-04-15 12:46:07 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
 # Before we invert matrix we have to use function makeCacheMatrix() on our matrix.
-# Function makeCacheMatrix() assigns to dx the list of functions on matrix which we want to invert in list dx.
+# Function makeCacheMatrix() assigns to dx the list of functions on matrix which we want to invert in list - dx.
 # dx is assigned globally and than it is later used by function cacheSolve(), which exactly inverts our matrix.
 
 # Function makeCacheMatrix() 
 # It assigns globally to variable df the list of functions on a matrix to invert.
-# df consist of three functions: get() return the matrix to invert, getsolve() stores in "m" the inverted matrix.
-# "m" is stored in makecacheMatrix() function,
+# df consist of three functions:
+#get() which returns the matrix to invert,
+#getsolve() which stores in "m" the inverted matrix. Notice that "m" is stored in makecacheMatrix() function.
 #setsolve() is a function which calculates the inverted matrix and stores it in "m"
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -23,7 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 #This function firstly checks if matrix was inverted (!is.null(m)).
 # Then returns the "m", where inverted matrix is stored or it calculates it.
 # If CacheSolve() computes inverted matrix, firstly, it gets matrix with get(), then it inverts it,
-#and finally it stores it in m variable (remember, that m is stord in makeCacheMatrix()).
+#and finally it stores it in m variable (remember, that m is stored in makeCacheMatrix()).
 
 
 cacheSolve <- function(x, ...) {
